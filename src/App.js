e12e1fcd955a6611877bbcb58182276ced472af8
Velliton <evg92faut@gmail.com>
2023-09-17 00:26:41 +0700

    code-link

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import './App.css';
 import ReactPlayer from 'react-player';
 import {Card} from 'antd';
-import myImg from './assets/2.JPG'
-import myImg1 from './assets/test.png'
-import myVideo from './assets/yandex.mp4'
+import myImg from './assets/11grade.JPG'
+import myImg1 from './assets/now.png'
+import myVideo from './assets/flexbox.mp4'
 
 const { Meta } = Card;
+
+const customStyle = {
+  fontFamily:'Poppins, sans-serif',
+  width: '100%'
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <div className='CardWrapper'>
         <Card
-            
-            style={{ width: '100%' }}
+            style={customStyle}
             cover={<img alt="example" src={myImg} />}
           >
-            <Meta title="Это я в 11 классе, " description="Тогда я ещё не знал кем хочу быть и как сложится моя жизнь." />
+            <Meta title="Это я в 11 классе " description="Тогда я ещё не знал, кем хочу стать, и как сложится моя жизнь. Именно в то время я делал свои первые шаги в веб-разработке. Мне ещё предстояло принять главное решение: пойти учиться на программиста." />
         </Card>
       </div>
       <div className='CardWrapper'>
         <Card
-            
-            style={{ width: '100%' }}
+            style={customStyle}
             cover={<img alt="example" src={myImg1} />}
           >
-            <Meta title="Это я в 11 классе, " description="Тогда я ещё не знал кем хочу быть и как сложится моя жизнь." />
+            <Meta title="Снова я ;)" description="И вот позади годы учёбы. Первая работа в it-компании, затем смена сферы деятельности, чтобы понять, чем действительно хочу заниматься. Сейчас я разработчик в крупной it-компании." />
         </Card>
       </div>
       
-
-      <p>fdhfjdhfjhdjhfjdfhdjfhdjfh</p>
+      <div className='comment-box'>
+        <p> Благодаря программированию я получил возможность для творческой самореализациии. 
+          Программирование позволяет воплотить идеи в жизнь, что вдохновляет.   
+        </p>
+      </div>
+     
       <div className='PlayerWrapper'>
         <ReactPlayer 
           url={myVideo}
@@ -40,7 +49,11 @@ function App() {
         />
       </div>
       
-      <a href='https://codepen.io/pen/'>Попробуй в песочнице</a>
+      <div className='code-link'>
+        <a href='https://codepen.io/mguizsnx-the-bashful/pen/eYbGmvO' target='_blank'>Попробуй в песочнице</a>
+      </div>
+      
+
     </div>
   );
 }
