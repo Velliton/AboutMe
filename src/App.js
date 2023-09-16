76982e16c3d099b8e199d3f3d1d9a8a2e7ac940a
Velliton <evg92faut@gmail.com>
2023-09-16 22:57:35 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
+import {Card} from 'antd';
+import myImg from './assets/2.JPG'
+import myImg1 from './assets/test.png'
+import myVideo from './assets/yandex.mp4'
+
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='CardWrapper'>
+        <Card
+            
+            style={{ width: '100%' }}
+            cover={<img alt="example" src={myImg} />}
+          >
+            <Meta title="Это я в 11 классе, " description="Тогда я ещё не знал кем хочу быть и как сложится моя жизнь." />
+        </Card>
+      </div>
+      <div className='CardWrapper'>
+        <Card
+            
+            style={{ width: '100%' }}
+            cover={<img alt="example" src={myImg1} />}
+          >
+            <Meta title="Это я в 11 классе, " description="Тогда я ещё не знал кем хочу быть и как сложится моя жизнь." />
+        </Card>
+      </div>
+      
+
+      <p>fdhfjdhfjhdjhfjdfhdjfhdjfh</p>
+      <div className='PlayerWrapper'>
+        <ReactPlayer 
+          url={myVideo}
+          controls={true}
+          width='100%'
+          height='100%'
+        />
+      </div>
+      
+      <a href='https://codepen.io/pen/'>Попробуй в песочнице</a>
     </div>
   );
 }
